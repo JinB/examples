8cab6518fe8bc9891b4c1fabede6b2ab2cf029ab
JinB <jin@email.cz>
2023-04-13 15:36:31 +0200

    added use of local storage and error handling

--- converter-widget-app/src/components/Converter.jsx
+++ converter-widget-app/src/components/Converter.jsx
@@ -3,72 +3,116 @@ import { format } from 'date-fns'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { NumericFormat } from 'react-number-format'
+import { FaRegClock } from 'react-icons/fa'
 
 const API_URL = 'https://demo.4dates.net/rate?date='
 
 const Converter = () => {
-  const [date, setDate] = useState(new Date())
+  const [errorTxt, setErrorTxt] = useState()
+  const [date, setDate] = useState(
+    localStorage.getItem('date') !== null
+      ? new Date(localStorage.getItem('date'))
+      : new Date()
+  )
   const convertFromUSD = true
-  const [rate, setRate] = useState(21.45)
-  const [amount, setAmount] = useState(1)
+  const [rate, setRate] = useState(0)
+  const [amount, setAmount] = useState(
+    localStorage.getItem('amount') !== null ? localStorage.getItem('amount') : 1
+  )
   const [convertResult, setConvertResult] = useState(rate)
-  const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(true)
 
-  const fetchUrl = API_URL + format(date, 'dd.MM.yyyy')
-  //   console.log('CNB url: ' + fetchUrl)
+  const performConvert = (amount, rate) => {
+    localStorage.setItem('amount', amount)
+    const tmpRes = Math.round(amount * rate * 100) / 100
+    console.log(
+      `performConvert(): amount ${amount}, rate: ${rate}, result: ${tmpRes}`
+    )
+    setConvertResult(tmpRes)
+  }
+
   useEffect(() => {
     async function fetchData() {
       try {
-        // console.log('Try to reach CNB site...')
+        const fetchUrl = API_URL + format(date, 'dd.MM.yyyy')
+        console.log('fetchData(): ' + fetchUrl)
         const res = await fetch(fetchUrl)
-        // console.log('Got response:')
-        // console.log(res)
         // console.log('Response status: ' + res.status)
-        // console.log('Response status text: ' + res.statusText)
         const json = await res.json()
         // console.log('Got json:')
         console.log(json)
+        if (json.date !== format(date, 'dd.MM.yyyy')) {
+          throw Error(
+            'Impossible to get rate for ' + format(date, 'yyyy-MM-dd')
+          )
+        }
         setRate(json.rateUSD)
-        setConvertResult(Math.round(amount * json.rateUSD * 100) / 100)
       } catch (error) {
         console.log('Error: ' + error.message)
         console.log(error)
-        setError(error.message)
+        setErrorTxt(error.message)
       }
       setIsLoading(false)
     }
     fetchData()
   }, [date])
 
+  useEffect(() => {
+    if (!isLoading) {
+      performConvert(amount, rate)
+    }
+  }, [amount, rate, isLoading])
+
   return (
     <>
       <label htmlFor="datePicker">Rate fixing date:</label>
-      <DatePicker
-        id="datePicker"
-        selected={date}
-        dateFormat="yyyy-MM-dd"
-        maxDate={new Date()}
-        calendarStartDay={1}
-        onChange={(d) => setDate(d)}
-      />
-
+      <div>
+        <div className="divInRow">
+          <DatePicker
+            className="divInRow"
+            id="datePicker"
+            selected={date}
+            dateFormat="yyyy-MM-dd"
+            maxDate={new Date()}
+            calendarStartDay={1}
+            onChange={(d) => {
+              localStorage.setItem('date', d)
+              setDate(d)
+            }}
+          />
+        </div>
+        <div className="divInRow">
+          {format(date, 'yyyy-MM-dd') !== format(new Date(), 'yyyy-MM-dd') ? (
+            <FaRegClock
+              onClick={() => {
+                setDate(new Date())
+              }}
+              title="Reset to current date"
+            />
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
       <label htmlFor="amount">
-        ČNB rate USD/CZK: {isLoading ? '...' : rate}
+        ČNB rate USD/CZK: {isLoading || errorTxt ? '...' : rate}
       </label>
       <br />
-      <NumericFormat
-        id="amount"
-        value="1"
-        allowNegative={false}
-        thousandSeparator=","
-        suffix={convertFromUSD ? ' USD' : ' CZK'}
-        placeholder="Type some amount..."
-        onValueChange={(values, sourceInfo) => {
-          setAmount(values.floatValue)
-          setConvertResult(Math.round(values.floatValue * rate * 100) / 100)
-        }}
-      />
+      {errorTxt ? (
+        <div className="errorMsg">{errorTxt}</div>
+      ) : (
+        <NumericFormat
+          id="amount"
+          value={amount}
+          allowNegative={false}
+          thousandSeparator=","
+          suffix={convertFromUSD ? ' USD' : ' CZK'}
+          placeholder="Type some amount..."
+          onValueChange={(values, sourceInfo) => {
+            setAmount(values.floatValue)
+          }}
+        />
+      )}
       <br />
       {convertResult
         ? ` = ${convertResult.toLocaleString()} ${
